bfa69a5d0671451b3cb7c57ebecd5a3c1e16bef1
Shikha Shetty <shikhashetty15@gmail.com>
2024-05-14 22:39:31 +0530

    Add CRUD functionality to the Application

--- appointment-app/src/app/appointment-list/appointment-list.component.ts
+++ appointment-app/src/app/appointment-list/appointment-list.component.ts
@@ -1,10 +1,71 @@
 import { Component } from '@angular/core';
+import { Appointment } from '../models/appointment';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.css']
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit {
+  updateSuccessful: number | null = null;
+  newAppointmentTitle: string = "";
+  newAppointmentPerson: string = "";
+  newAppointmentDate: Date = new Date();
+  updatedAppointmentTitle: string = "";
+  updatedAppointmentPerson: string = "";
+  updatedAppointmentDate: Date = new Date();
+  selectedItemIndex: number | null = null;
+
+  appointments: Appointment[] = []
+
+  ngOnInit(): void {
+    let savedAppointments = localStorage.getItem("appointments");
+    this.appointments = savedAppointments ? JSON.parse(savedAppointments) : [];
+  }
+
+  addAppointment() {
+    if (this.newAppointmentTitle.trim().length && this.newAppointmentPerson.trim().length && this.newAppointmentDate) {
+      let newAppointment: Appointment = {
+        id: Date.now(),
+        title: this.newAppointmentTitle,
+        person: this.newAppointmentPerson,
+        date: this.newAppointmentDate
+      }
+
+      this.appointments.push(newAppointment)
+
+      this.newAppointmentTitle = "";
+      this.newAppointmentPerson = "";
+      this.newAppointmentDate = new Date();
+
+      localStorage.setItem("appointments", JSON.stringify(this.appointments))
+    }
+  }
+
+  deleteAppointment(index: number) {
+    this.appointments.splice(index, 1)
+    localStorage.setItem("appointments", JSON.stringify(this.appointments))
+  }
+
+  updateAppointment(index: number) {
+    this.selectedItemIndex = index;
+  }
+
+  cancelUpdate(index: number) {
+    this.updatedAppointmentTitle = "";
+    this.updatedAppointmentPerson = "";
+    this.updatedAppointmentDate = new Date();
+    this.selectedItemIndex = null;
+  }
+
+  saveAppointment(index: number) {
+    this.appointments[index].person = this.updatedAppointmentPerson;
+    this.appointments[index].title = this.updatedAppointmentTitle;
+    this.appointments[index].date = this.updatedAppointmentDate;
+    localStorage.setItem("appointments", JSON.stringify(this.appointments))
+    this.selectedItemIndex = null;
+    this.updateSuccessful = index;
+  }
 
 }
